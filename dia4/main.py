9c374ecef8368893e6677db5d49ed05ad3cb0098
LeonelPrestes-coder <leonelprestesestudos@gmail.com>
2024-10-07 20:28:11 -0300

    Dia 4

---
+++ dia4/main.py
@@ -0,0 +1,21 @@
+def adicao(x,y):
+    return x+y
+
+def subtracao(x,y):
+    return x-y
+
+def multiplicacao(x,y):
+    return x*y
+
+def divisao(x,y):
+    return x/y
+
+def calculadora():
+    print("Selecione a operação")
+    print("1.Adição")
+    print("2.Subtração")
+    print("3.Multiplição")
+    print("4.Divisão")
+
+num1 = int(input("Digite o primeiro número"))
+num2 = int(input("Digite o segundo número"))
