d617ee631e6bb2f12fbd267f935702d1cb3ab20e
LeonelPrestes-coder <leonelprestesestudos@gmail.com>
2024-10-07 22:10:07 -0300

    Dia 5

--- dia4/main.py
+++ dia4/main.py
@@ -11,11 +11,34 @@ def divisao(x,y):
     return x/y
 
 def calculadora():
-    print("Selecione a operação")
-    print("1.Adição")
-    print("2.Subtração")
-    print("3.Multiplição")
-    print("4.Divisão")
+  print("Selecione a operação.")
+  print("1.Adicao")
+  print("2.Subtracao")
+  print("3.Multiplicacao")
+  print("4.Divisao")
 
-num1 = int(input("Digite o primeiro número"))
-num2 = int(input("Digite o segundo número"))
+  while True:
+    escolha = input("Escolha(1/2/3/4): ")
+    if escolha in ('1','2','3','4'):
+        x = int(input("Digite o primeiro número"))
+        y = int(input("Digite o segundo número"))
+        if escolha == '1':
+            print("Resultado: ",adicao(x,y))
+        if escolha == '2':
+            print("Resultado: ",subtracao(x,y))
+        if escolha == '3':
+            print("Resultado: ",multiplicacao(x,y))
+        if escolha == '4':
+            if y != 0:
+                print("Resultado: ",divisao(x,y))
+            else:
+                print("Não é possivel a divisão por Zero")
+        else:
+            print("Escolha inválida")
+            
+        continuar = input("Deseja fazer outra operação?, (S/N)")
+        if continuar == "n" or "N":
+            print("Canculadora encerrada")
+            break
+        
+calculadora()
